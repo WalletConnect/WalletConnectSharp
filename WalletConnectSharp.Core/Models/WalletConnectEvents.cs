1dcefb66fb79d1803a6dcaff47133bddf71ad8da
John Morgan <john.morgan@infernored.com>
2022-08-19 22:41:40 -0400

    refactor WalletConnect Events

---
+++ WalletConnectSharp.Core/Models/WalletConnectEvents.cs
@@ -0,0 +1,9 @@
+namespace WalletConnectSharp.Core.Models;
+
+public static class WalletConnectStates
+{
+    public static readonly string SessionUpdate = "wc_sessionUpdate";
+    public static readonly string Connect = "connect";
+    public static readonly string Disconnect = "disconnect";
+    public static readonly string SessionFailed = "session_failed";
+}
