cf7c278bd956feae863e0d0be5476a07fbb80471
Julia Seward <giga.juwels@gmail.com>
2023-01-02 13:41:35 -0500

    finished documentation for Storage project

--- Core Modules/WalletConnectSharp.Storage/FileSystemStorage.cs
+++ Core Modules/WalletConnectSharp.Storage/FileSystemStorage.cs
@@ -8,11 +8,23 @@ using Newtonsoft.Json;
 
 namespace WalletConnectSharp.Storage
 {
+    /// <summary>
+    /// A Storage strategy that stores both an in-memory dictionary as well serialized/deserializes
+    /// all in-memory storage in a JSON file on the filesystem.
+    /// </summary>
     public class FileSystemStorage : InMemoryStorage
     {
+        /// <summary>
+        /// The file path to store the JSON file
+        /// </summary>
         public string FilePath { get; private set; }
         private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
         
+        /// <summary>
+        /// A new FileSystemStorage module that reads/writes all storage
+        /// values from storage
+        /// </summary>
+        /// <param name="filePath">The filepath to use, defaults to ~/.wc/store.json</param>
         public FileSystemStorage(string filePath = null)
         {
             if (filePath == null)
@@ -25,6 +37,11 @@ namespace WalletConnectSharp.Storage
             FilePath = filePath;
         }
 
+        /// <summary>
+        /// Initialize this storage module. Initialize the in-memory storage
+        /// as well as loads in the JSON file
+        /// </summary>
+        /// <returns></returns>
         public override Task Init()
         {
             return Task.WhenAll(
@@ -32,17 +49,34 @@ namespace WalletConnectSharp.Storage
             );
         }
 
+        /// <summary>
+        /// The SetItem function stores the value based on the specified key and type. Will
+        /// also update the JSON file
+        /// </summary>
+        /// <param name="key"> The key to store the value with</param>
+        /// <param name="value">The value to store</param>
+        /// <typeparam name="T">The type of data to store</typeparam>
         public override async Task SetItem<T>(string key, T value)
         {
             await base.SetItem<T>(key, value);
             await Save();
         }
+        
+        /// <summary>
+        /// The RemoveItem function deletes the value stored based off of the specified key.
+        /// Will also update the JSON file
+        /// </summary>
+        /// <param name="key">The key to delete the stored value pairing.</param>
         public override async Task RemoveItem(string key)
         {
             await base.RemoveItem(key);
             await Save();
         }
-
+        
+        /// <summary>
+        /// Clear all entries in this storage. WARNING: This will delete all data!
+        /// This will also update the JSON file
+        /// </summary>
         public override async Task Clear()
         {
             await base.Clear();
