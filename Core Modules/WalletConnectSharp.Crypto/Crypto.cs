1e1c2fd6400f062e02fade182a30f5c50778f37c
Julia Seward <giga.juwels@gmail.com>
2023-02-21 23:34:49 -0500

    chore: update nuget info for project files

--- Core Modules/WalletConnectSharp.Crypto/Crypto.cs
+++ Core Modules/WalletConnectSharp.Crypto/Crypto.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using Merkator.BitCoin;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
