859eb6d319886b050240f699441917b85768c2ce
Julia Seward <giga.juwels@gmail.com>
2023-05-01 01:03:24 -0400

    feat: remove ext, add optional ns + session props

--- WalletConnectSharp.Sign/Models/Namespace.cs
+++ WalletConnectSharp.Sign/Models/Namespace.cs
@@ -6,12 +6,24 @@ namespace WalletConnectSharp.Sign.Models
     /// A namespace that holds accounts, methods and events enabled. Also includes
     /// extension namespaces that are enabled
     /// </summary>
-    public class Namespace : BaseNamespace
+    public class Namespace
     {
         /// <summary>
-        /// Any extension namespaces that are enabled in this namespace
+        /// An array of all accounts enabled in this namespace
         /// </summary>
-        [JsonProperty("extension", NullValueHandling=NullValueHandling.Ignore)]
-        public BaseNamespace[] Extension { get; set; }
+        [JsonProperty("accounts")]
+        public string[] Accounts { get; set; }
+        
+        /// <summary>
+        /// An array of all methods enabled in this namespace
+        /// </summary>
+        [JsonProperty("methods")]
+        public string[] Methods { get; set; }
+        
+        /// <summary>
+        /// An array of all events enabled in this namespace
+        /// </summary>
+        [JsonProperty("events")]
+        public string[] Events { get; set; }
     }
 }
