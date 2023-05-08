b5bb95816333f6b1b6a580862dd51a0374bfff34
Julia Seward <giga.juwels@gmail.com>
2023-05-07 20:42:43 -0400

    feat: init auth

---
+++ WalletConnectSharp.Auth/Internals/IssDidUtils.cs
@@ -0,0 +1,44 @@
+﻿namespace WalletConnectSharp.Auth.Internals
+{
+    public static class IssDidUtils
+    {
+        public static string[] ExtractDidAddressSegments(string iss)
+        {
+            if (string.IsNullOrWhiteSpace(iss))
+                return null;
+
+            return iss.Split(":");
+        }
+
+        public static string DidChainId(string iss)
+        {
+            if (string.IsNullOrWhiteSpace(iss))
+                return null;
+
+            return ExtractDidAddressSegments(iss)[3];
+        }
+
+        public static string NamespacedDidChainId(string iss)
+        {
+            if (string.IsNullOrWhiteSpace(iss))
+                return null;
+
+            var segments = ExtractDidAddressSegments(iss);
+
+            return $"{segments[2]}:{segments[3]}";
+        }
+
+        public static string DidAddress(string iss)
+        {
+            if (string.IsNullOrWhiteSpace(iss))
+                return null;
+
+            var segments = ExtractDidAddressSegments(iss);
+
+            if (segments.Length == 0)
+                return null;
+
+            return segments[segments.Length - 1];
+        }
+    }
+}
