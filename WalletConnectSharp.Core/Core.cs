cf5175cfbaa9285ccd0152020843bbb9bb03bde7
Julia Seward <giga.juwels@gmail.com>
2023-06-12 16:35:13 -0400

    chore: mark Core class as obsolute

---
+++ WalletConnectSharp.Core/Core.cs
@@ -0,0 +1,5 @@
+﻿namespace WalletConnectSharp.Core
+{
+    [Obsolete("This class has been renamed to WalletConnectCore, and will be removed in 2.1.x.", false)]
+    public class Core : WalletConnectCore {  }
+}
