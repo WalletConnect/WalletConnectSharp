d96fd2c97fcb69cd228aa64c908932458619b64b
Julia Seward <giga.juwels@gmail.com>
2023-07-20 01:49:21 -0400

    test: #77

--- Tests/WalletConnectSharp.Sign.Test/SignTests.cs
+++ Tests/WalletConnectSharp.Sign.Test/SignTests.cs
@@ -4,6 +4,7 @@ using WalletConnectSharp.Network.Models;
 using WalletConnectSharp.Sign.Interfaces;
 using WalletConnectSharp.Sign.Models;
 using WalletConnectSharp.Sign.Models.Engine;
+using WalletConnectSharp.Tests.Common;
 using Xunit;
 
 namespace WalletConnectSharp.Sign.Test
@@ -11,12 +12,22 @@ namespace WalletConnectSharp.Sign.Test
     public class SignTests : IClassFixture<SignClientFixture>
     {
         private SignClientFixture _cryptoFixture;
+        private const string AllowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
         [RpcMethod("test_method"), RpcRequestOptions(Clock.ONE_MINUTE, 99998)]
         public class TestRequest
         {
             public int a;
             public int b;
+        }
+        
+        [RpcMethod("test_method_2"), 
+         RpcRequestOptions(Clock.ONE_MINUTE, 99997), 
+         RpcResponseOptions(Clock.ONE_MINUTE, 99996)]
+        public class TestRequest2
+        {
+            public string x;
+            public int y;
         }
 
         [RpcResponseOptions(Clock.ONE_MINUTE, 99999)]
@@ -253,6 +264,123 @@ namespace WalletConnectSharp.Sign.Test
             Assert.Equal(eventResult, a * b);
             Assert.Equal(eventResult, testData.a * testData.b);
             Assert.Equal(eventResult, responseReturned.result);
+        }
+        
+        [Fact, Trait("Category", "integration")]
+        public async void TestTwoUniqueSessionRequestResponse()
+        {
+            await _cryptoFixture.WaitForClientsReady();
+            
+            var testAddress = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+            var testMethod = "test_method";
+            var testMethod2 = "test_method_2";
+
+            var dappConnectOptions = new ConnectOptions()
+            {
+                RequiredNamespaces = new RequiredNamespaces()
+                {
+                    {
+                        "eip155", new ProposedNamespace()
+                        {
+                            Methods = new[]
+                            {
+                                testMethod,
+                                testMethod2
+                            },
+                            Chains = new[]
+                            {
+                                "eip155:1"
+                            },
+                            Events = new[]
+                            {
+                                "chainChanged", "accountsChanged"
+                            }
+                        }
+                    }
+                }
+            };
+
+            var dappClient = ClientA;
+            var connectData = await dappClient.Connect(dappConnectOptions);
+
+            var walletClient = ClientB;
+            var proposal = await walletClient.Pair(connectData.Uri);
+
+            var approveData = await walletClient.Approve(proposal, testAddress);
+
+            var sessionData = await connectData.Approval;
+            await approveData.Acknowledged();
+
+            var rnd = new Random();
+            var a = rnd.Next(100);
+            var b = rnd.Next(100);
+            var x = rnd.NextStrings(AllowedChars, (Math.Min(a, b), Math.Max(a, b)), 1).First();
+            var y = x.Length;
+
+            var testData = new TestRequest() { a = a, b = b, };
+            var testData2 = new TestRequest2() { x = x, y = y };
+
+            var pending = new TaskCompletionSource<int>();
+            var pending2 = new TaskCompletionSource<bool>();
+            
+            // Step 1. Setup event listener for request
+            
+            // The wallet client will listen for the request with the "test_method" rpc method
+            walletClient.Engine.SessionRequestEvents<TestRequest, TestResponse>()
+                    .OnRequest += ( requestData) =>
+                {
+                    var request = requestData.Request;
+                    var data = request.Params;
+
+                    requestData.Response = new TestResponse()
+                    {
+                        result = data.a * data.b
+                    };
+                    
+                    return Task.CompletedTask;
+                };
+            
+            // The wallet client will listen for the request with the "test_method" rpc method
+            walletClient.Engine.SessionRequestEvents<TestRequest2, bool>()
+                .OnRequest += ( requestData) =>
+            {
+                var request = requestData.Request;
+                var data = request.Params;
+
+                requestData.Response = data.x.Length == data.y;
+                    
+                return Task.CompletedTask;
+            };
+
+            // The dapp client will listen for the response
+            // Normally, we wouldn't do this and just rely on the return value
+            // from the dappClient.Engine.Request function call (the response Result or throws an Exception)
+            // We do it here for the sake of testing
+            dappClient.Engine.SessionRequestEvents<TestRequest, TestResponse>()
+                .FilterResponses((r) => r.Topic == sessionData.Topic)
+                .OnResponse += (responseData) =>
+            {
+                var response = responseData.Response;
+                
+                var data = response.Result;
+
+                pending.TrySetResult(data.result);
+
+                return Task.CompletedTask;
+            };
+            
+            // 2. Send the request from the dapp client
+            var responseReturned = await dappClient.Engine.Request<TestRequest, TestResponse>(sessionData.Topic, testData);
+            var responseReturned2 = await dappClient.Engine.Request<TestRequest2, bool>(sessionData.Topic, testData2);
+            
+            // 3. Wait for the response from the event listener
+            var eventResult = await pending.Task.WithTimeout(TimeSpan.FromSeconds(5));
+            
+            Assert.Equal(eventResult, a * b);
+            Assert.Equal(eventResult, testData.a * testData.b);
+            Assert.Equal(eventResult, responseReturned.result);
+            
+            Assert.True(responseReturned2);
         }
     }
 }
---
+++ Tests/WalletConnectSharp.Tests.Common/UtilExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace WalletConnectSharp.Tests.Common;
+
+public static class UtilExtensions
+{
+    public static IEnumerable<string> NextStrings(
+        this Random rnd,
+        string allowedChars,
+        (int Min, int Max)length,
+        int count)
+    {
+        ISet<string> usedRandomStrings = new HashSet<string>();
+        (int min, int max) = length;
+        char[] chars = new char[max];
+        int setLength = allowedChars.Length;
+    
+        while (count-- > 0)
+        {
+            int stringLength = rnd.Next(min, max + 1);
+    
+            for (int i = 0; i < stringLength; ++i)
+            {
+                chars[i] = allowedChars[rnd.Next(setLength)];
+            }
+    
+            string randomString = new string(chars, 0, stringLength);
+    
+            if (usedRandomStrings.Add(randomString))
+            {
+                yield return randomString;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+}
