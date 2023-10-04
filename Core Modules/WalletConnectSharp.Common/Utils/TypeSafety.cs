571923636407feda941c5d295e7cadb6b4656112
Julia Seward <giga.juwels@gmail.com>
2023-10-04 18:46:36 -0400

    chore: use same settings as storage serializer

--- Core Modules/WalletConnectSharp.Common/Utils/TypeSafety.cs
+++ Core Modules/WalletConnectSharp.Common/Utils/TypeSafety.cs
@@ -13,12 +13,15 @@ public static class TypeSafety
         // to / from JSON should tell us
         // if it's serializer safe, since
         // we are using the serializer to test
-        UnsafeJsonRewrap<T, T>(testObject);
+        UnsafeJsonRewrap<T, T>(testObject, Settings);
     }
 
-    public static TR UnsafeJsonRewrap<T, TR>(this T source)
+    public static TR UnsafeJsonRewrap<T, TR>(this T source, JsonSerializerSettings settings = null)
     {
-        var json = JsonConvert.SerializeObject(source);
+        var json = settings == null ? 
+            JsonConvert.SerializeObject(source) : 
+            JsonConvert.SerializeObject(source, settings);
+        
         return JsonConvert.DeserializeObject<TR>(json);
     }
 }
